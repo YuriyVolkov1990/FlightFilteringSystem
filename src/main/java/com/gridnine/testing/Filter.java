786cb841fe90d6272fdcaf9dcdd783d2b5d65cbe
Yuriy <new99kpicem@gmail.com>
2024-01-14 23:37:15 +0300

    prc

--- src/main/java/com/gridnine/testing/Filter.java
+++ src/main/java/com/gridnine/testing/Filter.java
@@ -2,6 +2,9 @@ package com.gridnine.testing;
 
 import java.util.List;
 
+/**
+ * Интерфейс содержит метод для фильтрации полётов
+ */
 interface Filter {
    List<Flight> filter(List<Flight> flights);
 }
