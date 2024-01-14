0f7ecf08395848faba77163ac624b515be1f702c
Yuriy <new99kpicem@gmail.com>
2024-01-14 12:52:00 +0300

    prc

--- src/main/java/com/gridnine/testing/GroundTimeMoreThanTwoHours.java
+++ src/main/java/com/gridnine/testing/GroundTimeMoreThanTwoHours.java
@@ -5,11 +5,13 @@ import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ *  Исключение перелётов, где общее время, проведённое на земле, превышает два часа
+ */
 public class GroundTimeMoreThanTwoHours implements Filter {
     @Override
     public List<Flight> filter(List<Flight> flights) {
         List<Flight> filteredFlights = new ArrayList<>();
-
         work:
         for (int i = 0; i < flights.size(); i++) {
             long groundTimeCounter = 0;
@@ -18,7 +20,6 @@ public class GroundTimeMoreThanTwoHours implements Filter {
                 Segment segment = flight.getSegments().get(j);
                 Segment nextSegment = flight.getSegments().get(j + 1);
                 groundTimeCounter += segment.getArrivalDate().until(nextSegment.getDepartureDate(), ChronoUnit.HOURS);
-                System.out.println("groundTimeCounter = " + groundTimeCounter);
                 if (groundTimeCounter > 2) {
                     continue work;
                 }
